db2e1c4fff2b3bf9c00da67754820bdd12de640b
Ubuntu <ubuntu@ip-172-31-90-197.ec2.internal>
2019-07-22 17:03:34 +0000

    Continue testing Google API on oldindex.html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,36 @@
-// Google Places API
+var map;
+
+// Create map function: activateMapSearch.
+function activateMapSearch() {
+    var options = {
+        center: { lat: 43.3878549, lng: 5.960352 },
+        zoom: 9,
+        disableDefaultUI: true
+    };
+    
+    map = new google.maps.Map(document.getElementById('map'), options);
+    
+    // Create the search box and link it to the map.
+    var input = /** @type {HTMLInputElement} */(document.getElementById('search'));
+    
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
+
+    // [START region_getplaces]
+    // Listen for the event fired: when user selects an item
+    // from the search box, and retrieve matching places for that item on the map.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+            }
+    });
+}
+
+// Call the function and display the map on the browser.
+google.maps.event.addDomListener(window, 'load', activateMapSearch);
