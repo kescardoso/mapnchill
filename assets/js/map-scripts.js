785d888699e4836891c4fea3be5948ac3e36fb6b
Ubuntu <ubuntu@ip-172-31-90-197.ec2.internal>
2019-08-15 14:07:48 +0000

    Testing map fitBounds and zoom

--- assets/js/map-scripts.js
+++ assets/js/map-scripts.js
@@ -1,116 +1,86 @@
-// // Google API:
-
-// var map;
-// var service;
-// var infowindow;
-
-// // Create map function: activateMapSearch.
-// function activateMapSearch() {
-    
-//     var coteAzur = new google.maps.LatLng(43.5587661,6.354539);
-//     var markers = [];
-    
-//     infowindow = new google.maps.InfoWindow();
-    
-//     map = new google.maps.Map(document.getElementById('map'),
-//         {center: coteAzur, zoom: 9, disableDefaultUI: true}
-//     );
-    
-//     // Display yoga services with markers on the map.    
-//     var request = {
-//         location: center,
-//         radius: 10000,
-//         types: ['yoga']
-//     };
-    
-//     var service = new google.maps.places.PlacesService(map);
-    
-//     service.nearbySearch(request, callback);
-// }
-
-// // Callback function: returns good results avoiding errors.
-// function callback(results, status) {
-//     if(status == google.maps.places.PlacesServicesStatus.OK){
-//         for (var i = 0; i < results.length; i++){
-//             createMarker(results[i]);
-//         }
-//     }
-// }
-
-// // createMarker function: display markers on the map for yoga servcies.
-// function createMarker(place) {
-//     var placeLoc = place.geometry.location;
-//     var marker = new google.maps.Marker({
-//         map: map,
-//         position: place.geometry.location
-//     });
-// }
-
-// // Call the function and display the map on the browser.
-// google.maps.event.addDomListener(window, 'load', activateMapSearch);
-
-
-// Escapes HTML characters in a template literal string, to prevent XSS.
-// See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
-function sanitizeHTML(strings) {
-  const entities = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'};
-  let result = strings[0];
-  for (let i = 1; i < arguments.length; i++) {
-    result += String(arguments[i]).replace(/[&<>'"]/g, (char) => {
-      return entities[char];
-    });
-    result += strings[i];
-  }
-  return result;
-}
+// Google API:
+// Create map function: activateMapSearch.
+var map;
+var coteAzur = {lat: 43.8019903, lng: 6.6057867};
 
 function activateMapSearch() {
 
-  // Create the map.
-  const map = new google.maps.Map(document.getElementsByClassName('map')[0], {
-    zoom: 9,
-    center: {lat: 43.5587661, lng: 6.354539},
-  });
+    var markers = [];
+    var mapOptions = {
+		zoom: 9,
+		center: coteAzur,
+		mapTypeId: 'roadmap'
+    }
+    
+    infowindow = new google.maps.InfoWindow();
+    
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      // google.maps.event.addListener(map, 'bounds_changed', function() {
+		    // alert(map.getBounds());
+      // });
+	
+// 	var defaultBounds = new google.maps.LatLngBounds(
+//       new google.maps.LatLng(43.583560899329235, 5.818945233984323),
+//       new google.maps.LatLng(44.1774634389157, 7.755285566015573));
+//     map.fitBounds(defaultBounds);
+        
+    // Create the search box and link it to the UI element.
+    var input = /** @type {HTMLInputElement} */(
+      document.getElementById('search'));
+    map.controls[google.maps.ControlPosition.TOP].push(input);
 
-  // Load the GeoJSON data onto the map.
-  map.data.loadGeoJson('assets/json/yoga-mass.json');
+    var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+    // [START region_getplaces]
+    // Listen for the event fired: when user selects an item
+    // from the search box, and retrieve matching places for that item on the map.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+            }
+    
+    // For each place, get icon, name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
   
-  // Define the custom marker icons, using the store's "category".
-  map.data.setStyle(feature => {
-    return {
-      icon: {
-        url: `img/icon_${feature.getProperty('category')}.png`,
-        scaledSize: new google.maps.Size(64, 64)
-      }
-    };
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
   });
-  
-  const apiKey = 'AIzaSyBmNn5vKRp_5ZR_NBvqMtN-IWyVJeCafTk';
-  const infoWindow = new google.maps.InfoWindow();
-  infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
-
-  // Show the information for a store when its marker is clicked.
-  map.data.addListener('click', event => {
-
-    const category = event.feature.getProperty('category');
-    const name = event.feature.getProperty('name');
-    const services = event.feature.getProperty('services');
-    const address = event.feature.getProperty('address');
-    const website = event.feature.getProperty('website');
-    const position = event.feature.getGeometry().get();
-    const content = sanitizeHTML`
-      <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
-      <div style="margin-left:220px; margin-bottom:20px;">
-        <h2>${name}</h2><p>${address}</p>
-        <p><b>Available services:</b> ${services}<br/></p>
-        <p><b>Website:</b> ${website}<br/></p>
-        <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}"></p>
-      </div>
-    `;
-
-    infoWindow.setContent(content);
-    infoWindow.setPosition(position);
-    infoWindow.open(map);
-  });
-
 }
+
+// Call the function and display the map on the browser.
+google.maps.event.addDomListener(window, 'load', activateMapSearch);
