b3501208513c43603f0857341c64b17240266a01
kescardoso <kesia.development@gmail.com>
2019-08-28 20:55:25 +0000

    Edit searchbox code to be linked to map (not working yet)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,66 +27,64 @@ function activateMap() {
 	map.data.setStyle(function(feature) {
     return {icon:feature.getProperty('icon')};
   });
-        
-  
 
-
-  // // Create the search box and link it to the UI element.
-  // var input = /** @type {HTMLInputElement} */(
-  //   document.getElementById('search'));
-  // map.controls[google.maps.ControlPosition.TOP].push(input);
-
-  // var searchBox = new google.maps.places.SearchBox(
-  //   /** @type {HTMLInputElement} */(input));
-  
-  // // [START region_getplaces]
-  // // Listen for the event fired: when user selects an item
-  // // from the search box, and retrieve matching places for that item on the map.
-  // google.maps.event.addListener(searchBox, 'places_changed', function() {
-  //   var places = searchBox.getPlaces();
-
-  //   if (places.length == 0) {
-  //     return;
-  //   }
-  //   for (var i = 0, marker; marker = markers[i]; i++) {
-  //     marker.setMap(null);
-  //   }
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+    document.getElementById('search'));
     
-  //   // For each place, get the icon, place name, and location.
-  //   markers = [];
-  //   var bounds = new google.maps.LatLngBounds();
-  //   for (var i = 0, place; place = places[i]; i++) {
-  //     var image = {
-  //       url: place.icon,
-  //       size: new google.maps.Size(71, 71),
-  //       origin: new google.maps.Point(0, 0),
-  //       anchor: new google.maps.Point(17, 34),
-  //       scaledSize: new google.maps.Size(25, 25)
-  //     };
+  map.controls[google.maps.ControlPosition.TOP].push(input);
 
-  //     // Create a marker for each place.
-  //     var marker = new google.maps.Marker({
-  //       map: map,
-  //       icon: image,
-  //       title: place.name,
-  //       position: place.geometry.location
-  //     });
-
-  //     markers.push(marker);
-
-  //     bounds.extend(place.geometry.location);
-  //   }
-
-  //   map.fitBounds(bounds);
-  // });
-  // //[END region_getplaces]
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
   
-  // // Bias the SearchBox results towards places that are within the bounds of the
-  // // current map's viewport.
-  // google.maps.event.addListener(map, 'bounds_changed', function() {
-  //   var bounds = map.getBounds();
-  //   searchBox.setBounds(bounds);
-  // });
+  // [START region_getplaces]
+  // Listen for the event fired: when user selects an item
+  // from the search box, and retrieve matching places for that item on the map.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+    
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+  //[END region_getplaces]
+  
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
 }
 
 // Call the function and display the map on the browser.
@@ -102,12 +100,12 @@ google.maps.event.addDomListener(window, 'load', activateMap);
 
 
 // // Google API:
-// // Create map function: activateMapSearch.
+// // Create map
 // var map;
 // var service;
 // var infowindow;
 
-// function activateMapSearch() {
+// function activateMap() {
     
 //     var coteAzur = new google.maps.LatLng(43.5587661,6.354539);
 //     var markers = [];
