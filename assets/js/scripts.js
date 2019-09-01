73ea1dc1d6add00f036b6092bf595d34eddec3df
kescardoso <kesia.development@gmail.com>
2019-09-01 16:47:40 +0000

    Start new map code and test loadJSON function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,47 @@
-// Google API
-
+//Assign global variables
+var google;
+var map;
+var coteAzur = {lat: 43.8691025, lng: 7.2054375};
+var mapOptions = {
+	zoom: 10,
+	center: coteAzur,
+	mapTypeId: 'roadmap'
+};
+// Define custom map markers
+var icons = {
+  yogaIcon: {
+    icon: 'assets/images/yoga-icon.png'
+  },
+  massageIcon: {
+    icon: 'assets/images/massage-icon.png'
+  }
+};
 
 
 // Create map
 function activateMap() {
   
-  var map;
-  var coteAzur = {lat: 43.8691025, lng: 7.2054375};
-  var mapOptions = {
-		zoom: 10,
-		center: coteAzur,
-		mapTypeId: 'roadmap'
-  }
+  // // Load GeoJson data from map.json
+  // function loadJSON(callback) {   
+
+  //   var xobj = new XMLHttpRequest();
+  //       xobj.overrideMimeType("application/json");
+  //   xobj.open('GET', 'assets/json/map.json', true);
+  //   xobj.onreadystatechange = function () {
+  //         if (xobj.readyState == 4 && xobj.status == "200") {
+  //           callback(xobj.responseText);
+  //         }
+  //   };
+  //   xobj.send(null); 
+  // }
   
+  // var loadedGeoJson;
+  // loadJSON(function(response) {
+  //   loadedGeoJson = JSON.parse(response);
+  //     var geoJSON = JSON.parse(this.responseText);
+  //     map.data.addGeoJson(geoJSON );
+  //   });
+    
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
   
