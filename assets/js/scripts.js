a6c2963398fb11df94752ce40a61d3a341c91ce1
kescardoso <kesia.development@gmail.com>
2019-09-01 17:25:06 +0000

    Add data via scripts.js for testing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,23 +7,60 @@ var mapOptions = {
 	center: coteAzur,
 	mapTypeId: 'roadmap'
 };
-// Define custom map markers
-var icons = {
-  yogaIcon: {
-    icon: 'assets/images/yoga-icon.png'
+
+// Define custom icons for map markers
+var icons = { 
+  yoga: { 
+    icon: 'https://www.dropbox.com/s/2xypunn2g7t72gy/markerMassage.png'
   },
-  massageIcon: {
-    icon: 'assets/images/massage-icon.png'
+  massage:
+  {
+    icon: 'https://www.dropbox.com/s/9c40fslmwf24y2e/markerYoga.png'
   }
 };
 
+//Add yoga and massage businesses to map
+var businesses = [
+  {
+    type: 'yoga',
+    icon: 'yoga',
+    title: 'Kes Cardoso',
+    content: 'http://www.kescardoso.com',
+    position: 
+    {
+      lat: 43.7186366,
+      lng: 7.2706853
+    }
+  }, 
+  {
+    type: 'yoga',
+    icon: 'yoga',
+    title: 'Eva Luna',
+    content: 'http://facebook.com',
+    position: 
+    {
+      lat: 43.6227815,
+      lng: 7.0463002
+    }
+  },
+  {
+    type: 'yoga',
+    icon: 'yoga',
+    title: 'PepitaYoga',
+    content: 'http://www.pepitayoga.com',
+    position:
+    {
+      lat: 43.6984938,
+      lng: 7.2571126
+    }
+  },
+];
 
 // Create map
 function activateMap() {
   
   // // Load GeoJson data from map.json
   // function loadJSON(callback) {   
-
   //   var xobj = new XMLHttpRequest();
   //       xobj.overrideMimeType("application/json");
   //   xobj.open('GET', 'assets/json/map.json', true);
@@ -34,15 +71,32 @@ function activateMap() {
   //   };
   //   xobj.send(null); 
   // }
-  
   // var loadedGeoJson;
   // loadJSON(function(response) {
   //   loadedGeoJson = JSON.parse(response);
   //     var geoJSON = JSON.parse(this.responseText);
   //     map.data.addGeoJson(geoJSON );
   //   });
-    
+  
+  //Load Map  
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  
+  //Add markers and display infowindows on mouseover
+  var InfoWindows = new google.maps.InfoWindow({});
+	
+	businesses.forEach(function(business) {	
+		var marker = new google.maps.Marker({
+		  position: { lat: business.position.lat, lng: business.position.lng },
+		  map: map,
+		  icon: icons[business.icon].icon,
+		  title: business.title
+		});
+		marker.addListener('mouseover', function() {
+		  InfoWindows.open(map, this);
+		  InfoWindows.setContent(business.content);
+		});
+	});
+
 }
   
 // // Call the function and display the map on the browser.
