16c32ca68e9803c8109ef70c872d2c65e0292545
kescardoso <kesia.development@gmail.com>
2019-09-17 18:36:31 +0000

    Add favicon and home screen buttons for mobile and web browsers

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,46 +6,46 @@ var features = [
   //YOGA BUSINESSES:
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.kescardoso.com">KES CARDOSO<br/>Yoga & Sound</a></h6> <p><strong>Yoga:</strong> Yin-Yang Balanced Flow, Hatha Yoga Flow, Kundalini Yoga, Yin Yoga Restorative.<br/><strong>Massage:</strong> Sound Healing Therapy, Sonic Massage.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.kescardoso.com">KES CARDOSO<br/>Yoga & Sound</a></h6> <p><strong>Yoga:</strong> Yin-Yang Balanced Flow, Hatha, Kundalini, Yin/Restorative.<br/><strong>Sound Therapy:</strong> Sound Healing Therapy and Sonic Massage.</p> </div>',
     position: { lat: 43.71875279296885, lng: 7.2729331254959115 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.pepitayoga.com">Pepita Yoga</a></h6> <p>Hatha Flow, Acro Yoga, Hot Yoga, Power Yoga, Yin Yoga, Pilates.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.pepitayoga.com">Pepita Yoga</a></h6> <p>Hatha, Vinyasa, AcroYoga, HotYoga, PowerYoga, Yin/Restorative, Pilates.</p> </div>',
     position: { lat: 43.69852262776876, lng: 7.2591894865036 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="https://enso.fr/">Enso Yoga Massena</a></h6> <p>Hatha Yoga, Iyengar, Ashtanga, Vinyasa, Restorative, Pre-natal.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://enso.fr/">Enso Yoga Massena</a></h6> <p>Hatha, Iyengar, Ashtanga, Vinyasa, Restorative, Pre-natal.</p> </div>',
     position: { lat: 43.69755689999653, lng: 7.2676920890808105 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="https://enso.fr/">Enso Yoga Valbonne</a></h6> <p>Hatha Yoga, Iyengar, Ashtanga, Vinyasa, Restorative, Pre-natal.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://enso.fr/">Enso Yoga Valbonne</a></h6> <p>Hatha, Iyengar, Ashtanga, Vinyasa, Restorative, Pre-natal.</p> </div>',
     position: { lat: 43.62272781889663, lng: 7.04847514629364 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.rivierayoga.fr">Riviera Yoga</a></h6> <p>Hatha Yoga, Ashtanga & Vinyasa Yoga, Restorative, Pre/Post-natal.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.rivierayoga.fr">Riviera Yoga</a></h6> <p>Hatha, Ashtanga, Vinyasa, Restorative, Pre/Post-natal.</p> </div>',
     position: { lat: 43.69970552613459, lng: 7.2816047072410575 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="https://www.facebook.com/peaceyoganice/">Thousand Bridges (La Passarelle)</a></h6> <p>Bikram Yoga, Kundalini Yoga, Yin Yoga Restorative, Sound Healing, Nature Immersion and Retreats.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://www.facebook.com/peaceyoganice/">Thousand Bridges (La Passarelle)</a></h6> <p>Bikram, Kundalini, Yin/Restorative, Sound Healing, Nature Immersion and Retreats.</p> </div>',
     position: { lat: 44.077041901917, lng: 7.121543884277344 }
   },
   {
     icon: 'yoga',
-    content: '<div id="info-content"> <h6><a target="_blank" href="https://tigre-yoga.com/en/capsule/le-tigre-monte-carlo/">Le Tigre Monte Carlo</a></h6> <p>Hatha & Vinyasa Flow, Kundalini Yoga, Yin Yoga, Pilates.</p> </div>',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://www.mauna-meditation.com">Mauna Practice Center</a></h6> <p>Meditation, Hatha, Kundalini, Vinyasa, Gasquet, Hormonal Yoga, Sivananda, Kids Yoga, Pilates, Chinese Mouvement</p> </div>',
+    position: { lat: 43.60019467439821, lng: 7.093745470046997 }
+  },
+  {
+    icon: 'yoga',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://tigre-yoga.com/en/capsule/le-tigre-monte-carlo/">Le Tigre Monte Carlo</a></h6> <p>Hatha, Vinyasa, Kundalini, Yin/Restorative, Pilates.</p> </div>',
     position: { lat: 43.75085032982795, lng: 7.444299459457397 }
   },
 
   //MASSAGE BUSINESSES:
-  {
-    icon: 'massage',
-    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.kescardoso.com">KES CARDOSO<br/>Yoga & Sound</a></h6> <p><strong>Yoga:</strong> Yin-Yang Balanced Flow, Hatha Yoga Flow, Kundalini Yoga, Yin Yoga Restorative.<br/><strong>Massage:</strong> Sound Healing Therapy, Sonic Massage.</p> </div>',
-    position: { lat: 43.71873146899887, lng: 7.272777557373047 }
-  },
   {
     icon: 'massage',
     content: '<div id="info-content"> <h6><a target="_blank" href="http://www.bodywork.com">BodyWork by Andrea Daumas</a></h6> <p>Relaxing Massage, Reenergising/Detox Massage, Deep Tissue Massage, Thai Massage, Foot Reflexology.</p> </div>',
@@ -55,6 +55,25 @@ var features = [
     icon: 'massage',
     content: '<div id="info-content"> <h6><a target="_blank" href="https://rivieramassage.com/">Riviera Massage by Shona Marrs</a></h6> <p>Deep Tissue, Body Sculpting, Swedish, Sports, Acupressure, Thai, Skin rolling and manual kneading, Passive joint and sensory repatterning.</p> </div>',
     position: { lat: 43.58102233159652, lng: 7.122316360473633 }
+  },
+
+  //SOUND THERAPY
+  {
+    icon: 'soundtherapy',
+    content: '<div id="info-content"> <h6><a target="_blank" href="http://www.kescardoso.com">KES CARDOSO<br/>Yoga & Sound</a></h6> <p><strong>Yoga:</strong> Yin-Yang Balanced Flow, Hatha, Kundalini, Yin/Restorative.<br/><strong>Sound Therapy:</strong> Sound Healing Therapy and Sonic Massage.</p> </div>',
+    position: { lat: 43.71873146899887, lng: 7.272777557373047 }
+  },
+
+  //OSTEOPATHY
+  {
+    icon: 'osteopathy',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://lemedecin.fr/osteopathe/le-cannet/johanna-benzaquen/114-boulevard-sadi-carnot/rdv/new/"><p>Johanna Benzaquen (Cannes)</a></h6> <p>114 blvd Sadi Carnot 06110 - LE CANNET <br/>06 61 96 56 54</p> </div>',
+    position: { lat: 43.56541224689357, lng: 7.016299366950988 }
+  },
+  {
+    icon: 'osteopathy',
+    content: '<div id="info-content"> <h6><a target="_blank" href="https://www.doctolib.fr/osteopathe/nice/johanna-benzaquen">Johanna Benzaquen (Nice)</a></h6> <p>Johanna Benzaquen (Nice)</a></h6> <p>10 rue Alphonse Karr 06000 - Nice <br/>06 61 96 56 54</p> </div>',
+    position: { lat: 43.699100505623726, lng: 7.26568579673767 }
   }
 
 ];
@@ -71,7 +90,7 @@ var map;
 // Tutorial from: https://developers.google.com/maps/documentation/javascript/controls#Adding_Controls_to_the_Map
 var coteAzur = { lat: 43.8416215, lng: 7.2062602 };
 var mapOptions = {
-  zoom: 10,
+  zoom: 11,
   center: coteAzur,
   mapTypeId: 'roadmap',
   zoomControl: true,
@@ -94,7 +113,7 @@ var icons = {
   soundtherapy: {
     icon: 'assets/images/icon-sound.png'
   },
-  osteopathe: {
+  osteopathy: {
     icon: 'assets/images/icon-osteo.png'
   },
   pilates: {
